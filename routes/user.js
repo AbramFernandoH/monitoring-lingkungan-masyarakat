c169e9f43f69ff28d7ee64554f04c77ed139e998
Abram Fernando <abramfernando18@gmail.com>
2024-06-11 23:17:04 +0700

    add edit user, delete user, and pagination on list user

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const User = require('../model/user');
@@ -5,8 +6,18 @@ const { isLoggedIn, isAdmin } = require('../middleware');
 
 router.route('/')
     .get(isLoggedIn, isAdmin, async (req, res) => {
-        const listUser = await User.find({});
-        res.render('user', { headTitle: 'Warga', listUser });
+        const { skip } = req.query
+        const currentSkip = skip ? Number(skip) : 0
+        const currentUrl = process.env.ENVIROMENT === 'development' ? `${req.protocol}://${req.hostname}:${process.env.PORT}${req.path}user` : `${req.protocol}://${req.hostname}${req.path}user`
+
+        const query = {
+            admin: false
+        }
+        const limit = 10
+        const listUser = await User.find(query).limit(limit).skip(currentSkip);
+        const totalUser = await User.countDocuments(query);
+
+        res.render('user', { headTitle: 'Warga', listUser, totalUser, currentUrl, skip: currentSkip, limit });
     })
 
 router.route('/add')
@@ -14,21 +25,47 @@ router.route('/add')
         res.render('user/add-user', { headTitle: 'Tambah Warga' });
     })
     .post(isLoggedIn, isAdmin, async (req, res) => {
-        const form = req.body
+        const { IDNumber, headOfFamilyName, password } = req.body
         const user = new User({ 
-            IDNumber: Number(form.IDNumber),
-            headOfFamilyName: form.headOfFamilyName,
-            address: form.address,
-            username: form.headOfFamilyName,
+            ...req.body,
+            admin: false,
+            IDNumber: Number(IDNumber),
+            username: String(headOfFamilyName),
          });
-        await User.register(user, form.password);
+        await User.register(user, password);
         res.redirect('/user');
     })
 
 router.route('/edit/:userId')
     .get(isLoggedIn, isAdmin, async (req, res) => {
-        const editedUser = await User.findOne({ _id: req.params.userId })
+        const editedUser = await User.findById(req.params.userId)
         res.render('user/edit-user', { headTitle: 'Edit Warga', editedUser });
+    })
+    .patch(isLoggedIn, isAdmin, async (req, res) => {
+        try {
+            const editedUser = await User.findById(req.params.userId);
+            const { password, ...rest } = req.body;
+            
+            if (password.length > 0) {
+                await editedUser.setPassword(password);
+                await editedUser.save();
+            }
+
+            const result = await User.updateOne({ _id: req.params.userId }, rest);
+            res.redirect('/user');
+        } catch (e) {
+            res.redirect('/user');
+        }
+    })
+
+router.route('/delete/:userId')
+    .delete(isLoggedIn, isAdmin, async (req, res) => {
+        try {
+            await User.deleteOne({ _id: req.params.userId });
+            res.redirect('/user');
+        } catch (e) {
+            res.redirect('/user');
+        }
     })
 
 module.exports = router;
