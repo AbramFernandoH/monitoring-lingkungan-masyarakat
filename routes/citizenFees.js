d391694570d5402cbfd9b8309ea286fb1099d0af
Abram Fernando <abramfernando18@gmail.com>
2024-06-14 23:41:19 +0700

    add create & edit citizen fees feature

--- routes/citizenFees.js
+++ routes/citizenFees.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const { DateTime } = require('luxon');
 const CitizenFees = require('../model/citizenFees');
 const { isLoggedIn, isAdmin } = require('../middleware');
+const multer = require('multer');
+const { storage, cloudinary } = require('../cloudinary/cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(isLoggedIn, async (req, res) => {
@@ -93,27 +96,105 @@ router.route('/export')
         }
     })
 
-    router.route('/add')
+router.route('/add')
     .get(isLoggedIn, isAdmin, async (req, res) => {
-        const min=DateTime.now().toFormat('yyyy-LL-dd')
-        res.render('citizen-fees/add-iuran', { headTitle: 'Input Iuran',min });
+        const min = DateTime.now().toFormat('yyyy-LL-dd')
+        res.render('citizen-fees/add-iuran', { headTitle: 'Form Iuran', min });
     })
-    .post(isLoggedIn, isAdmin, async (req, res) => {
-        const form = req.body
-        const newcitizenfees = new CitizenFees({ 
-            ...req.body,
-            
-            owner: req.user,
-            months: [], 
-            images: []
+    .post(isLoggedIn, isAdmin, upload.array('images'), async (req, res) => {
+        const images = req.files.map(f => ({url: f.path, filename: f.filename}));
+
+        const getMonthRange = (startMonth, endMonth) => {
+            const startDate = DateTime.fromISO(startMonth + '-01');
+            const endDate = DateTime.fromISO(endMonth + '-01').endOf('month');
     
-         });
-         console.log(newcitizenfees,form)
-         await newcitizenfees.save()
+            let current = startDate.startOf('month');
+            const months = [];
+    
+            while (current <= endDate) {
+                months.push({ year: Number(current.toFormat('yyyy')), month: Number(current.toFormat('MM')) });
+                current = current.plus({ months: 1 });
+            }
+
+            return months;
+        }
+
+        const { startMonth, endMonth, ...form } = req.body
+
+        const newcitizenfees = new CitizenFees({ 
+            ...form,
+            images,
+            owner: req.user,
+            months: endMonth ? getMonthRange(startMonth, endMonth) : [
+                {
+                    year: DateTime.fromISO(startMonth + '-01').toFormat('yyyy'),
+                    month: DateTime.fromISO(startMonth + '-01').toFormat('MM'),
+                },
+            ],
+        });
+
+         await newcitizenfees.save();
+
          res.redirect('/citizen-fees');
+    });
 
+router.route('/edit/:feesId')
+    .get(isLoggedIn, isAdmin, async (req, res) => {
+        const editedFees = await CitizenFees.findById(req.params.feesId)
+
+        const startMonth = `${editedFees.months[0].year}-${editedFees.months[0].month < 10 ? `0${editedFees.months[0].month}` : editedFees.months[0].month}`
+        const endMonth = editedFees.months.length > 1 ? `${editedFees.months[editedFees.months.length - 1].year}-${editedFees.months[editedFees.months.length - 1].month < 10 ? `0${editedFees.months[editedFees.months.length - 1].month}` : editedFees.months[editedFees.months.length - 1].month}` : ''
+
+        const dateOfTransaction = DateTime.fromJSDate(editedFees.dateOfTransaction).toFormat('yyyy-MM-dd')
+
+        res.render('citizen-fees/edit-iuran', { headTitle: 'Form Iuran', editedFees, startMonth, endMonth, dateOfTransaction });
     })
+    .patch(isLoggedIn, isAdmin, upload.array('images'), async (req, res) => {
+        const { startMonth, endMonth, deleteImg, ...form } = req.body
 
+        const fees = await CitizenFees.findByIdAndUpdate({ _id: req.params.feesId }, { ...form });
+        const images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+        fees.images.push(...images);
+
+        const getMonthRange = (startMonth, endMonth) => {
+            const startDate = DateTime.fromISO(startMonth + '-01');
+            const endDate = DateTime.fromISO(endMonth + '-01').endOf('month');
+    
+            let current = startDate.startOf('month');
+            const months = [];
+    
+            while (current <= endDate) {
+                months.push({ year: Number(current.toFormat('yyyy')), month: Number(current.toFormat('MM')) });
+                current = current.plus({ months: 1 });
+            }
+
+            return months;
+        }
+
+        fees.months = endMonth ? getMonthRange(startMonth, endMonth) : [
+            {
+                year: DateTime.fromISO(startMonth + '-01').toFormat('yyyy'),
+                month: DateTime.fromISO(startMonth + '-01').toFormat('MM'),
+            },
+        ]
+
+        await fees.save();
+
+        if (deleteImg) {
+            if(deleteImg.length > 1){
+                for(let filename of deleteImg){
+                    // delete images from cloudinary
+                    await cloudinary.uploader.destroy(filename);
+                }
+            } else {
+                await cloudinary.uploader.destroy(deleteImg[0]);
+            }
+            // delete images from mongoose
+            await fees.updateOne({ $pull: { images: { filename: { $in: deleteImg } } } });
+        }
+
+         res.redirect('/citizen-fees');
+    });
 
 router.route('/delete/:feesId')
     .delete(isLoggedIn, isAdmin, async (req, res) => {
